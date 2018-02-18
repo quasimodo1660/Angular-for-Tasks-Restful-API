e254c1a5d77704f381f0703b2908b9331413af2b
Quasimodo <quasimodo1660@gmail.com>
2018-02-18 10:08:44 -0800

    Create observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { HttpService } from './http.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  tasks=[];
+  title = 'appsbbbbb';
+  constructor(private _httpService:HttpService){}
+  ngOnInit(){
+    this.getTasksFromService();
+  }
+  getTasksFromService(){
+    let observable = this._httpService.getTasks();
+    observable.subscribe(data => {console.log("Got our tasks!", data)
+    this.tasks = data['tasks']
+    console.log(this.tasks);
+    });  
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { HttpService } from './http.service';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 
@@ -10,9 +10,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
